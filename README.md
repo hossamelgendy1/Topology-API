a602e431031a2480c823a42bda5251470d56df01
hossamelgendy1 <59685428+hossamelgendy1@users.noreply.github.com>
2022-05-11 16:55:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Topology-API
+ An API that provides the functionality to access, manage and store device topologies.
---
+++ src/test/java/TopologyAPI/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package TopologyAPI.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
